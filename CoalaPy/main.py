ae16bea41226c96c9988a5b2c239069113a318de
Federico Ziegler <105019871+fedex2118@users.noreply.github.com>
2023-02-16 11:27:11 +0100

    Update main.py

--- CoalaPy/main.py
+++ CoalaPy/main.py
@@ -17,6 +17,6 @@ def main(filename: str):
 if __name__ == "__main__":
     # how this works: 
     #
-    # main("AmoCoala/Datasets/RealDatasets/AP_coala.nex")
+    # main("Coala/Datasets/RealDatasets/AP_coala.nex")
     
     main("Write here your nexus file path")
