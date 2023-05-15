0fc79aa107362219aff893becc1e85a999589cf2
Federico Ziegler <105019871+fedex2118@users.noreply.github.com>
2023-05-15 12:01:59 +0200

    Delete myParser.py

--- myParser.py
+++
@@ -1,8 +0,0 @@
-import argparse as arg
-import sys
-
-class MyParser(arg.ArgumentParser):
-    def error(self, message):
-        sys.stderr.write("error: %s\n" % message)
-        self.print_help()
-        sys.exit(2)
